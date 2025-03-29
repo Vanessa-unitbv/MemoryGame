93897c17fc21b837cf1f1e814507ccb1c8a6b68b
Vanessa-unitbv <vanessa.palatka@student.unitbv.ro>
2025-03-29 16:54:46 +0200

    Am realizat folderele pentru MVVM si am adaugat pozele.png pentru Users.

--- MemoryGame/Views/MainWindow.xaml.cs
+++ MemoryGame/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MemoryGame
+namespace MemoryGame.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
